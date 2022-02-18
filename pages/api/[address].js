4928736e597e0958f02b03be189f1854f7d03f2d
wcatz <waynecataldo@gmail.com>
2022-02-18 08:32:30 -0500

    post

---
+++ pages/api/[address].js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+// import axios from 'axios' // es6
+
+async function getData({ address }) {
+  try {
+    const response = await axios.request({
+      method: "get",
+      url: "https://vmtest.adaseal.eu/api.php",
+      params: {
+        action: "get_reward_breakdown",
+        token:
+          "58f4f1d3e8f8a5c1f9d1de211fc4e1804342281136d1a18babfce52f621d0072",
+        staking_address: address,
+      },
+    });
+
+    console.log("response data", response.data);
+    return response.data;
+  } catch (e) {
+    console.log("error: ", e);
+
+    if (e?.response?.data) {
+      console.log("error response data:", e.response.data);
+      return e.response.data;
+    }
+    return e.message;
+  }
+}
+
+export default async function handler(req, res) {
+  const { address } = req.query;
+
+  const data = await getData({ address });
+  console.log(req.query);
+  res.status(200).json(data);
+}
